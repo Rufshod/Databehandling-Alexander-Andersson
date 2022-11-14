11b6e85a44a015a9be0b599cd65053995eaaf220
Rufshod <alexander@11a.se>
2022-11-14 11:05:52 +0100

    refactored into separate layout module

--- Code-alongs/L5.1-stocky_dashboard/main.py
+++ Code-alongs/L5.1-stocky_dashboard/main.py
@@ -1,81 +1,39 @@
-import os
-import sys
 import dash
-from load_data import StockData
-from time_filtering import filter_time
-import plotly_express as px
-import pandas as pd
-from dash import html, dcc  # dcc - dash core components
 import dash_bootstrap_components as dbc
-from dash.dependencies import Output, Input # Needed to use callbacks. If user clicks on something, something should happen.
+import os
+from load_data import StockData
+from dash import html, dcc  # dcc - dash core components
+from dash.dependencies import Output, Input
+import plotly_express as px
+from time_filtering import filter_time
+import pandas as pd
+from layout import Layout # importing layout class from layout.py
 
-# find absolute path in vscode, when clicking run button it will check where you are in terminal, 
-# which is in Databehandling AI 222
 directory_path = os.path.dirname(__file__)
 path = os.path.join(directory_path, "stocksdata")
-sys.path.append(os.path.join(directory_path,"graphs"))
-
 
 print(path)
-# instantiate an object from the class
-# StockData that we have imported
+
 stockdata_object = StockData(path)
 
-# pick one stock (to test)
+# pick one stock
 # print(stockdata_object.stock_dataframe("AAPL"))
 
 symbol_dict = {"AAPL": "Apple", "NVDA": "Nvidia", "TSLA": "Tesla", "IBM": "IBM"}
 
 df_dict = {symbol: stockdata_object.stock_dataframe(symbol) for symbol in symbol_dict}
 
-stock_options_dropdown = [
-    {"label": name, "value": symbol} for symbol, name in symbol_dict.items()
-]
+print(df_dict.keys())
+# print(df_dict["TSLA"][0])
 
-slider_marks = {
-    i: mark
-    for i, mark in enumerate(
-        ["1 day", "1 week", "1 month", "3 months", "1 year", "5 years", "Max"]
-    )
-}
+# create a Dash App
+app = dash.Dash(__name__)
 
-ohlc_options = [
-    {"label": option, "value": option} for option in ("open", "high", "low", "close")
-]
-# print(df_dict.keys()) # See the keys in df_dict
-# print(df_dict["TSLA"][0]) #Get the Tesla Dataframe
-
-# create a Dash App.
-app = dash.Dash(__name__) # boilerplate code
-
-
-app.layout = html.Main( # This is a html method, 
-    [
-        html.H1("Knock off Avanza"),  # html H1 = Header 1 (rubrik)
-        html.P("Choose a stock"),
-        dcc.Dropdown(
-            id="stockpicker-dropdown",
-            options=stock_options_dropdown,
-            value="AAPL",
-        ),
-        html.P(id="highest-value"),
-        html.P(id="lowest-value"),
-        dcc.RadioItems(
-            id="ohlc-radio", options=ohlc_options, value="close"
-        ),  # creates the "button" object.
-        dcc.Graph(id="stock-graph"),
-        dcc.Slider(
-            id="time-slider", min=0, max=6, marks=slider_marks, value=2, step=None
-        ),  # value = 2 = 1 month, step allows us to snap between values.
-        # storing intermediate value on clients browser in order to share between several callbacks.
-        dcc.Store(id="filtered-df"),
-    ]
-)
-
+app.layout = Layout(symbol_dict).layout() # using imported class.
 
 @app.callback(
     Output("filtered-df", "data"),
-    Input("stockpicker-drowpdown", "value"),
+    Input("stockpicker-dropdown", "value"),
     Input("time-slider", "value"),
 )
 def filter_df(stock, time_index):
@@ -91,16 +49,16 @@ def filter_df(stock, time_index):
 
 
 @app.callback(
-    Output("highest_value", "children"),
-    Output("lowest_value", "children"),
-    Input("filtered-df", "data"), # when this is changed function below will run.
-    Input("olhc-radio", "value"),# same as above.
+    Output("highest-value", "children"),
+    Output("lowest-value", "children"),
+    Input("filtered-df", "data"),
+    Input("ohlc-radio", "value"),
 )
 def highest_lowest_value_update(json_df, ohlc):
     dff = pd.read_json(json_df)
     highest_value = dff[ohlc].max()
     lowest_value = dff[ohlc].min()
-    return f"Highest value {highest_value}, Lowest value {lowest_value}"
+    return highest_value, lowest_value
 
 
 @app.callback(
